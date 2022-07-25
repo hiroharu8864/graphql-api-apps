78a4ee690514402060a6ba9485ad1b7182a00226
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-25 14:54:32 +0900

    一旦別リポジトリで動作確認する

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages/Home";
 import { GraphQLFetch } from "../components/pages/GraphQLFetch";
 import { PassValue } from "../components/pages/PassValue";
-import { RecoilRequest } from "../components/pages/RecoilRequest";
-import { RecoilResult } from "../components/pages/RecoilResult";
 
 export const Router: FC = memo(() => {
   return (
@@ -13,8 +11,6 @@ export const Router: FC = memo(() => {
         <Route path="/" element={<Home />} />
         <Route path="/fetch" element={<GraphQLFetch />} />
         <Route path="/passvalue" element={<PassValue />} />
-        <Route path="/recoilrequest" element={<RecoilRequest />} />
-        <Route path="/recoilresult" element={<RecoilResult />} />
       </Routes>
     </BrowserRouter>
   );
--- src/store/userStoreRepositotyName.js
+++ src/store/userStoreRepositotyName.js
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const reposState = atom({
+export const userStoreRepositotyName = atom({
   key: "userStoreRepositotyName",
   defalult: ""
 });
